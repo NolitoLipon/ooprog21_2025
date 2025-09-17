354df6df8657435e248b216fa2154bc06912a475
LiponNolito-UCM <nolito.lipon04@gmail.com>
2025-09-17 23:06:20 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Same Ground,");
+        System.out.println("Oo,");
+        System.out.println("Make it with you,");
+        System.out.println("Won't Go Home Without YOU");
+   }
+}
